20708576e3fcff900399ed5a747dfd459c7b87a0
ysnblt <106676214+ysnblt@users.noreply.github.com>
2022-09-28 20:42:32 +0300

    Add files via upload

---
+++ Classlar/Classlar/Araba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar
+{
+    public class Araba
+    {
+        public int kapiSayisi;
+        public string arabaModel;
+        public string arabaRengi;
+        public void motorCalıstır()
+        {
+            Console.WriteLine("Motor çalışıyor");
+        }
+
+
+        public Araba(int _kapiSayisi , string _arabaModeli , string _arabaRengi )
+        {
+            kapiSayisi = _kapiSayisi;
+            arabaModel = _arabaModeli;
+            arabaRengi = _arabaRengi;
+
+        }
+
+        public void kapilariKilitle()
+        {
+            Console.WriteLine("Kapılar kilitleniyor");
+        }
+    }
+}
---
+++ Classlar/Classlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kapiSayisi = 4;
+            string arabaModeli = "Renault";
+            string arabaRengi = "Kırmızı";
+            Araba araba1 = new Araba(4 , "Renault" , "Kırmızı");
+            Console.WriteLine("Kapı sayısı :"  + araba1.kapiSayisi);
+            Console.WriteLine("Araba modeli :" + araba1.arabaModel);
+            Console.WriteLine("Araba rengi :" + araba1.arabaRengi);
+           
+            Console.ReadLine();
+        }
+    }
+}
